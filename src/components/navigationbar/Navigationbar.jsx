4af86aa244e3b9616798c63be44bd885f2719ec2
Tuomas Tanskanen <tuomastanskanen@protonmail.com>
2024-04-17 21:43:21 +0300

    Created  assets

--- src/components/navigationbar/Navigationbar.jsx
+++ src/components/navigationbar/Navigationbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navigationbar.css';
 
 const Navigationbar = () => {
   return (
-    <div>Navigationbar</div>
+    <div className='gpt__navigationbar'>
+      <div className='gpt__navigationbar-links'>
+        <div className='gpt__navigationbar-links__logo'>
+          <img src={logo} alt="logo"></img>
+        </div>
+      </div>
+    </div>
   )
 }
 
