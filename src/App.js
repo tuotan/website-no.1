a5fe9a4ad25686f8b3c154a744cc29d808041de5
Tuomas Tanskanen <tuomastanskanen@protonmail.com>
2024-04-17 21:49:19 +0300

    Edited files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+
+import { Blog, Features, Footer, GPT, Header, Possibility } from './containers';
+import { Brand, CTA, Navigationbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>
-        <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navigationbar />
+        <Header />
+      </div>
+      <Brand />
+      <GPT />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
