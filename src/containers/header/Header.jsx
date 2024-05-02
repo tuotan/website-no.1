a97a14b0d46b9ed0fb92f656fd1bae6ecfeaaa21
Tuomas Tanskanen <tuomastanskanen@protonmail.com>
2024-05-02 09:47:19 +0300

    Edited header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,7 +3,10 @@ import './header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt__header section padding' id='home'>
+      <div className='gpt__header-content'></div>
+      <h1 className='gradient__text'>Hello GPT</h1>
+    </div>
   )
 }
 
