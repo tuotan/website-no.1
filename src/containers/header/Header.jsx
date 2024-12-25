954411f86763c0fb3cfbb13465aa8cb8970a02cd
Tuomas Tanskanen <tuomastanskanen@protonmail.com>
2024-12-25 21:30:42 +0200

    Edited header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
       </div>
 
       <div className='gpt__header-content__people'>
-        <img src={1} alt='Hello' />
+        <img src={12} alt='Hello' />
         <p>Place</p>
       </div>
     </div>
